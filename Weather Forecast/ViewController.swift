e64c808769f39db1692773fb5ac0ef0e9c1527ec
Shaoze Pan <shaozepan@shaozetekimacbook-pro.local>
2016-07-09 21:15:02 -0400

    Initial Commit

---
+++ Weather Forecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather Forecast
+//
+//  Created by Shaoze Pan on 2016-07-09.
+//  Copyright © 2016 FriedChicken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
